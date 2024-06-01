2786c59964d7cb10c3523bb9773c4dc267aff4c0
yourunb <bxyura@gmail.com>
2024-06-02 01:36:48 +0300

    feat: add list to items

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,11 @@ sectionMoney.classList.add('section-money');
 
 sectionMoney.innerHTML = `
   <h3 class="section-money__title">Зарабатывайте вместе с нами!</h3>
+  <ul class="list">
+    <li class="list__item"></li>
+    <li class="list__item"></li>
+    <li class="list__item"></li>
+  </ul>
 `;
 
 main.append(sectionMoney);
