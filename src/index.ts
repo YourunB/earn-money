a207f90011865844d388683388c784bd92036a90
yourunb <bxyura@gmail.com>
2024-06-02 10:30:22 +0300

    feat: add numbers and description for all cards

--- src/index.ts
+++ src/index.ts
@@ -15,14 +15,26 @@ sectionMoney.innerHTML = `
   <h3 class="section-money__title">Зарабатывайте вместе с нами!</h3>
   <ul class="list">
     <li class="list__item">
+      <div class="item-header">
+        <span class="item-header__number item-header__number_color1">1</span>
+        <span class="item-header__text item-header__text_width1">Получите партнерскую ссылку</span>
+      </div>
       <img class="list__arrow" src="arrow.svg" alt="Arrow">
       <img class="list__image" src="card1.png" alt="Info">
     </li>
     <li class="list__item">
+      <div class="item-header">
+        <span class="item-header__number item-header__number_color2">2</span>
+        <span class="item-header__text item-header__text_width2">Расскажите о IIII.COM</span>
+      </div>
       <img class="list__arrow" src="arrow.svg" alt="Arrow">
       <img class="list__image" src="card2.png" alt="Info">
     </li>
     <li class="list__item">
+      <div class="item-header">
+        <span class="item-header__number item-header__number_color3">3</span>
+        <span class="item-header__text item-header__text_width3">Получайте хороший процент от покупок по вашей ссылке!</span>
+      </div>
       <img class="list__image" src="card3.png" alt="Info">
     </li>
   </ul>
